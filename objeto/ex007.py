020783112637011823a184d338649cf14cc2ddb5
neanthertal-albino <iagogeneral2@gmail.com>
2026-06-20 13:17:33 -0300

    Update ex007.py

--- objeto/ex007.py
+++ objeto/ex007.py
@@ -11,23 +11,62 @@ class Controle:
         self.turn = False
         self.canal = 1
         self.volume = 1
+        self.max_vol = 5
+        self.min_vol = 0
+        self.max_canal = 5
+        self.barra = ""
+        self.texto_canais = ""
 
         while True:
             if self.turn == False:
                 console.print(Panel(':heavy_multiplication_x: A TV está [red]desligada[/]!', title="[ TV ]"))
             else:
-                console.print(Panel("[blue]Canal[/]  = [black on yellow]1[/] 2 3 4 5\n[blue]Volume[/] = [black on yellow]1[/] 2 3 4 5", title="[ TV ]"))  
+                console.print(Panel(f"[blue]Canal[/]  = {self.texto_canais}\n[blue]Volume[/] = {self.barra}", title="[ TV ]"))  
 
-            self.control = input("< CH > - VOL + ").strip()
-            print("\n"*8)
+            self.control = input(f"< CH{self.canal} > - VOL{self.volume} + ").strip()
+            print("\n"*10)
 
             if self.control == "0":
                 break
-                
+
             if self.control == "@" and self.turn == False:
                 self.turn = True
+
             elif self.control == "@" and self.turn == True:
                 self.turn = False
+
+            if self.turn == True: 
+                if self.control == "+" and self.volume < self.max_vol:
+                    self.volume += 1
+
+                elif self.control == '-' and self.volume > self.min_vol:
+                    self.volume -= 1
+
+
+                if self.control == ">":
+                    if self.canal == self.max_canal:
+                        self.canal = 1
+                    else:
+                        self.canal += 1
+                
+                elif self.control == "<":
+                    if self.canal == 1:
+                        self.canal = self.max_canal
+                    else:
+                        self.canal -= 1
+
+
+            self.barra = "█" * self.volume + "░" * (self.max_vol - self.volume)
+
+            self.texto_canais = ""
+
+            for c in range(1, self.max_canal + 1):
+                if c == self.canal:
+                    self.texto_canais += f"[black on yellow] {c} [/]"
+                else:
+                    self.texto_canais += f" {c} "
+
+
 
 
 c = Controle()
