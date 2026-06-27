e5b6b4208e0be74e7fa44dbfa666c10ed9f4d87a
neanthertal-albino <iagogeneral2@gmail.com>
2026-06-27 14:06:34 -0300

    Update model.py

--- objeto/outros/pessoa/model.py
+++ objeto/outros/pessoa/model.py
@@ -2,14 +2,26 @@ from abc import ABC
 from datetime import date, datetime
 
 class Pessoa():
-    def __init__(self, n):
+    def __init__(self, n, i):
         self.nome = n
-        self.__idade = 0
+        self.__idade = i
         
     @property
     def idade(self):
-        pass
+        return self.__idade
 
 
-a = Pessoa('Carlos')
-print(a.ano(2000))
+class Aluno(Pessoa):
+    def __init__(self,n, i, m):
+        super().__init__(n, i)
+        self.matricula = m
+
+    def mostrar(self):
+        return (
+            f'Aluno: {self.nome}'
+            f'\nIdade: {self.idade}'
+            f'\nMatrícula: {self.matricula}'
+        )
+
+a = Aluno("carlos", 23, 12345677)
+print(a.mostrar())
